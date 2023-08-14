0fbeec92ee93a2384b00347c56f2b43482b30ce8
maryh12 <nickymary0@gmail.com>
2023-08-13 22:07:22 -0300

    Add código php do painel

---
+++ painel.php
@@ -0,0 +1,24 @@
+<?php
+
+include('protect.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="Western (ISO 8859-1)">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="protect.css">
+        <title>Painel</title>
+    </head>
+
+    <body>
+        Bem vindo ao Painel, <?php echo $_SESSION['nome']; ?>.
+
+        <p>
+            <a href="logout.php">Sair</a>
+        </p>
+    </body>
+</html>
