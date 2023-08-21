89b8534a47546964e9bac6a977c916855eea7597
maryh12 <103159989+maryh12@users.noreply.github.com>
2023-08-21 09:27:01 -0300

    Atualização cadastro e inserindo cadastro.php

---
+++ cadastro/cadastro.php
@@ -0,0 +1,31 @@
+<?php
+    include_once "cod_conexao.php";
+
+    $nome = $_POST['nome'];
+    $dataNascimento = $_POST['datadenascimento'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $celular = $_POST['celu'];
+    $cpf = $_POST['cpf'];
+    $telefone1 = $_POST['telefone1'];
+    $logradouro = $_POST['logradouro'];
+    $numero = $_POST['numero'];
+    $complemento = $_POST['complemento'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $cep = $_POST['cep'];
+    $genero = $_POST['genero'];
+
+    $sql = "INSERT INTO cliente (Nome, DataDeNascimento, CPF, Genero, Email, Senha, Logradouro, Numero, Complemento, Bairro, Cidade, Estado, CEP, Telefone_1, Telefone_2) values('$nome', '$dataNascimento', '$cpf', '$genero', '$email', '$senha', '$logradouro', '$numero', '$complemento', '$bairro', '$cidade', '$estado', '$cep', '$telefone1', '$celular')";
+
+    $resultado = mysqli_query($conexao, $sql);
+
+    if ($resultado === FALSE)
+        echo "Erro na inclusão do registro..." . mysqli_error($conexao) . "</br>";
+    else
+        echo mysqli_affected_rows($conexao) . " registro incluído com sucesso!</br>";
+
+    mysqli_close($conexao);
+
+?>
